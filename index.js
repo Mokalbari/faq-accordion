46b5899c27e66fe4593132b8bb63b54cac1126fe
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-05-20 17:25:44 +0200

    feat: icon changes depending on classList("hidden")

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ const toggleFaq = (() => {
     content2: document.querySelector("#content2"),
     content3: document.querySelector("#content3"),
     content4: document.querySelector("#content4"),
+
+    img1: document.querySelector("#toggleIcon1"),
+    img2: document.querySelector("#toggleIcon2"),
+    img3: document.querySelector("#toggleIcon3"),
+    img4: document.querySelector("#toggleIcon4"),
   };
 
   const getIconNumber = (target) => {
@@ -33,6 +38,12 @@ const toggleFaq = (() => {
       : (targetID = "");
     const number = getIconNumber(targetID);
     const content = `content${number}`;
+    const currentImg = `img${number}`;
+
     contentToDisplay[content].classList.toggle("hidden");
+
+    contentToDisplay[content].classList.contains("hidden")
+      ? (contentToDisplay[currentImg].src = "./assets/images/icon-plus.svg")
+      : (contentToDisplay[currentImg].src = "./assets/images/icon-minus.svg");
   });
 })();
